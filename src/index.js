6b2e6f91b3838350cbde8807a0959af8aa97c280
ayoubzann <ayoub@gmail.com>
2023-01-17 15:54:38 +0100

    Added fetch to frontend

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import App from "./Components/App"
 import "./styles.css"
 import { BrowserRouter } from "react-router-dom"
 
+
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
   <React.StrictMode>
